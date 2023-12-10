c9107d94dd610427f9cfde503cf61d66c7968c96
vatratabaku <vatratabakuu@gmail.com>
2023-12-10 23:32:46 +0100

    new changes

--- src/components/AboutLayout.jsx
+++ src/components/AboutLayout.jsx
@@ -3,12 +3,14 @@ import "./css/aboutLayout.css";
 import { motion } from "framer-motion";
 import ExperiencesSection from "./ExperiencesSection";
 import ProjectsSection from "./ProjectsSection";
+import DetailsSection from "./DetailsSection";
 
 function AboutLayout() {
   return (
     <>
       <div className="about-container">
         <Navbar></Navbar>
+       <DetailsSection></DetailsSection>
         <div className="first-row">
           <motion.h1
             initial={{ y: 250, opacity: 0, scaleX: 0 }}
---
+++ src/components/DetailsSection.jsx
@@ -0,0 +1,29 @@
+import "../components/css/detailsSection.css"
+
+
+function DetailsSection() {
+  return (
+    <div className="details-container">
+      <h1>ABOUT ME</h1>
+      <div className="wrapper">
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+            corrupti necessitatibus, nemo placeat earum inventore similique
+            ratione ullam aperiam ab sed quod magnam dignissimos totam optio
+            quas ipsam culpa. Reprehenderit. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Suscipit aliquam, aliquid deserunt
+            rerum eveniet tempora dicta aperiam. Repellat nesciunt eos
+            perspiciatis maxime accusantium, quo dolor, quaerat iste animi
+            incidunt a. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dolores iusto alias nam nemo voluptas praesentium corporis id, natus
+            numquam non distinctio ab sed dicta hic eum soluta, ducimus,
+            necessitatibus iure.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DetailsSection;
