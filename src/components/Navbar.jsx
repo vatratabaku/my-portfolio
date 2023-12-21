0cef075f94c6dd178b12e6aa83987baf8081a088
vatratabaku <vatratabakuu@gmail.com>
2023-12-22 00:39:22 +0100

    new change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
             className={({ isActive, isPending }) =>
               isPending ? "pending" : isActive ? "active-link" : "link"
             }
-            to="/"
+            to="/my-portfolio"
           >
             Home
           </NavLink>
